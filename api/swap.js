5a80b9dd3064563910959c90abbf9128e1de7105
biofractal <biofractal@gmail.com>
2017-08-24 15:05:33 +0100

    latest script changes

--- api/swap.js
+++ api/swap.js
@@ -3,20 +3,20 @@ var packageJson = require('./package.json')
 var fs = require('fs')
 var target = {}
 
-switch (packageJson.dependencyTarget) {
+switch (packageJson.swap.dependencyTarget) {
   case 'local':
-    target = packageJson.publishedDependencies
-    packageJson.dependencyTarget = 'published'
+    target = packageJson.swap.publishedDependencies
+    packageJson.swap.dependencyTarget = 'published'
     break
   case 'published':
-    target = packageJson.localDependencies
-    packageJson.dependencyTarget = 'local'
+    target = packageJson.swap.localDependencies
+    packageJson.swap.dependencyTarget = 'local'
     break
   default:
     throw new Error('incorrect dependencyTarget')
 }
 console.log('---------------------------------')
-console.info('Swapping Package References to: ', packageJson.dependencyTarget)
+console.info('Swapping Package References to: ', packageJson.swap.dependencyTarget)
 for (let name in target) {
   let value = target[name]
   console.log(`    ${name}:${value}`)
