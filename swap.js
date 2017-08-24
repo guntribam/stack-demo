e2abb63fcd0f4451d4b4518de52852aeda329e82
biofractal <biofractal@gmail.com>
2017-08-24 16:08:42 +0100

    test

--- swap.js
+++ swap.js
@@ -1,26 +1,29 @@
-var packageJson = require(`./${process.argv[2]}/package.json`)
-var {swap} = packageJson
-var fs = require('fs')
-var target = {}
+swap(`api`)
+swap(`app`)
+function swap (dir) {
+  var packageJson = require(`./${dir}/package.json`)
+  var {swap} = packageJson
+  var fs = require('fs')
+  var target = {}
 
-switch (swap.dependencyTarget) {
-  case 'local':
-    target = swap.publishedDependencies
-    swap.dependencyTarget = 'published'
-    break
-  case 'published':
-    target = swap.localDependencies
-    swap.dependencyTarget = 'local'
-    break
-  default:
-    throw new Error('incorrect dependencyTarget')
+  switch (swap.dependencyTarget) {
+    case 'local':
+      target = swap.publishedDependencies
+      swap.dependencyTarget = 'published'
+      break
+    case 'published':
+      target = swap.localDependencies
+      swap.dependencyTarget = 'local'
+      break
+    default:
+      throw new Error('incorrect dependencyTarget')
+  }
+
+  console.log(`-------------- ${swap.dependencyTarget} ----------------`)
+  for (let name in target) {
+    let value = target[name]
+    console.log(`${name}:${value}`)
+    packageJson.dependencies[name] = value
+  }
+  fs.writeFile(`../${dir}/package.json`, JSON.stringify(packageJson, null, '  '))
 }
-
-console.log(`-------------- ${swap.dependencyTarget} ----------------`)
-for (let name in target) {
-  let value = target[name]
-  console.log(`${name}:${value}`)
-  packageJson.dependencies[name] = value
-}
-
-fs.writeFile('./package.json', JSON.stringify(packageJson, null, '  '))
