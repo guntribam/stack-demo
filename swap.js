16077bd9b1c4716f22212aca2576464570266430
biofractal <biofractal@gmail.com>
2017-08-24 20:24:02 +0100

    swap improvements

--- swap.js
+++ swap.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 const { exec } = require('child_process')
+const projectPath = 'D:/code/stack/stack-demo'
 
 swap('app')
 swap('api')
 
 function swap (dir) {
-  const projectPath = `D:/code/stack/stack-demo/${dir}`
-  const ls = exec(`"sp-swap" ${projectPath}`, (error, stdout, stderr) => {
+  path = `${projectPath}/${dir}`
+  const ls = exec(`"sp-swap" ${path}`, (error, stdout, stderr) => {
     if (error) return console.error(`exec error: ${error}`)
     console.log(`${stdout}`)
     console.log(`${stderr}`)
