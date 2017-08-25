512744569859b9890919d233b91daf846075c324
biofractal <biofractal@gmail.com>
2017-08-25 11:21:10 +0100

    test

--- swap.js
+++ swap.js
@@ -1,18 +1,14 @@
 #!/usr/bin/env node
 const { exec } = require('child_process')
-const projectPath = 'D:/code/stack/stack-demo'
-
-console.info('process.argv', __dirname)
-
 console.log()
 
-// swap('app')
-// swap('api')
-//
-// function swap (dir) {
-//   const path = `${projectPath}/${dir}`
-//   const target = process.argv[2]
-//   exec(`"sp-swap" ${path} ${target}`, (error, stdout, stderr) => {
-//     console.log(`${stdout}`)
-//   })
-// }
+swap('app')
+swap('api')
+
+function swap (dir) {
+  const path = `${__dirname}/${dir}`
+  const target = process.argv[2]
+  exec(`"sp-swap" ${path} ${target}`, (error, stdout, stderr) => {
+    console.log(`${stdout}`)
+  })
+}
