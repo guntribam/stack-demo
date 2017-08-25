dae7d9d89a6cc135a282c91f2df33bf8a8482852
biofractal <biofractal@gmail.com>
2017-08-25 12:46:23 +0100

    test

--- swap.js
+++ swap.js
@@ -9,6 +9,10 @@ function swap (dir) {
   const path = `${__dirname}/${dir}`
   const target = process.argv[2]
   exec(`"sp-swap" ${path} ${target}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`)
+      return
+    }
     console.log(`${stdout}`)
     if (stderr) console.log(`${stdout}`)
   })
