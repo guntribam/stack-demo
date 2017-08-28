1a9051a39d83ac6f4befb68741784de098f35b15
biofractal <biofractal@gmail.com>
2017-08-28 10:44:33 +0100

    test

--- swap.js
+++ swap.js
@@ -1,19 +1,14 @@
 #!/usr/bin/env node
-const { exec } = require('child_process')
+const { spawn } = require('child_process')
 console.log()
 
 swap('app')
 swap('api')
 
 function swap (dir) {
-  const path = `${__dirname}/${dir}`
-  const target = process.argv[2]
-  exec(`sp-swap ${path} ${target}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`)
-      return
-    }
-    console.log(`${stdout}`)
-    if (stderr) console.log(`${stdout}`)
-  })
+  const env = {
+    path: `${__dirname}/${dir}`,
+    targetName: process.argv[2]
+  }
+  spawn(`sp-swap`, {stdio: 'inherit', shell: true, env})
 }
