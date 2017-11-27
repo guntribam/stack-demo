49e8a989704b64a24b4c0109a400f41359ffd234
biofractal <biofractal@gmail.com>
2017-11-27 11:50:22 +0000

    fetch updates

--- api/src/service/gp/processor.js
+++ api/src/service/gp/processor.js
@@ -1,8 +1,10 @@
 import gpapi from 'stack-pack-gpapi'
+
 import { makeProcessor } from '@gp-technical/stack-pack-api'
+
 const processor = async action => {
   var { types, type, data } = action
-  console.info('data', data)
+
   switch (type) {
     case types.gpGetDocuments:
       return await gpapi.get(`folder/${data}/items/simple`)
