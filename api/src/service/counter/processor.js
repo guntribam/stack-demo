3fd0c1a40f50a5ce7ae467d5defdadb566940fd3
biofractal <biofractal@gmail.com>
2017-12-12 21:24:32 +0000

    increment gets access to user from action

--- api/src/service/counter/processor.js
+++ api/src/service/counter/processor.js
@@ -2,9 +2,11 @@ import { makeProcessor } from '@gp-technical/stack-pack-api'
 import db from './db'
 
 const processor = async action => {
-  var { types, type, data } = action
+  var { types, type, data, user } = action
+
   switch (type) {
     case types.counterIncrement:
+      console.info('user', user)
       db.increment()
       return { total: db.getTotal() }
     case types.counterDecrement:
--- api/src/service/gp/initialiser.js
+++ api/src/service/gp/initialiser.js
@@ -3,8 +3,7 @@ import gpapi from 'stack-pack-gpapi'
 // Folder: Responsive Toolkit for Leaders_dev1_auto/Leadership & Strategy
 const folderId = '46c2f86d-0655-009b-86cc-a3bf00ac087a'
 
-const initialiser = async user => {
-  if (!user) return
+const initialiser = async () => {
   return await gpapi.get(`location/parent-folder/${folderId}/child-folders`)
 }
 
