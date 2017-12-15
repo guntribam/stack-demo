f2f511ca51ef43f7be38039703a14ade0cb442d0
Gabriel Araújo <gabrieldea.silva@gmail.com>
2017-12-15 12:09:38 -0200

    Remove log

--- api/src/service/shopping/initialiser.js
+++ api/src/service/shopping/initialiser.js
@@ -2,7 +2,6 @@ import db from './db'
 import cart from './cart'
 
 const initialiser = async (user) => {
-  console.info('user - initialiser', user)
   return {
     products: db.getProducts(),
     categories: db.getCategories(),
