c785a972246c65cdc137869892abf30fc053a6f8
Vinicius Paldês <vinnydp@gmail.com>
2017-12-23 11:19:43 -0200

    react-d3 chart lib test

--- app/src/component/charts/index.jsx
+++ app/src/component/charts/index.jsx
@@ -1,48 +1,55 @@
 import React from 'react'
 import { components } from '../../loader'
 import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from 'victory'
-import { BarChart, Bar, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts'
+// import { BarChart, Bar, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts'
 // import { XYPlot, LineSeries, VerticalGridLines, HorizontalGridLines, XAxis, YAxis } from 'react-vis'
+import { BarChart } from 'react-d3'
 
 class component extends React.PureComponent {
 
-  data = [
-    {name: 1, earnings: 1},
-    {name: 2, earnings: 2},
-    {name: 3, earnings: 3},
-    {name: 4, earnings: 0}
-  ];
   renderVictoryChart = () => {
+    const data = [
+      {name: 1, earnings: 1},
+      {name: 2, earnings: 2},
+      {name: 3, earnings: 3},
+      {name: 4, earnings: 0}
+    ]
     return (
+      <div>
+        <h3>Victory</h3>
         <VictoryChart width={730} height={250} theme={VictoryTheme.material} domainPadding={20}
         animate={{ duration: 1000, easing: 'bounce' }}>
-        <VictoryAxis tickValues={[1, 2, 3, 4]}
-          tickFormat={['Quarter 1', 'Quarter 2', 'Quarter 3', 'Quarter 4']}/>
-        <VictoryAxis dependentAxis tickFormat={(x) => (`$${x / 1}k`)}/>
-        <VictoryBar data={this.data} x="name" y="earnings"
-          style={{ data: { fill: 'blue', width: 40 } }} />
-      </VictoryChart>
+          <VictoryAxis tickValues={[1, 2, 3, 4]}
+            tickFormat={['Quarter 1', 'Quarter 2', 'Quarter 3', 'Quarter 4']}/>
+          <VictoryAxis dependentAxis tickFormat={(x) => (`$${x / 1}k`)}/>
+          <VictoryBar data={data} x="name" y="earnings"
+            style={{ data: { fill: 'blue', width: 40 } }} />
+        </VictoryChart>
+      </div>
     )
   }
 
-  rechartData = [
-    {name: 1, value: 1},
-    {name: 2, value: 2},
-    {name: 3, value: 3},
-    {name: 4, value: 0}
-  ];
-  renderRechart = () => {
-    return (
-      <BarChart width={730} height={250} data={this.rechartData}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" tickFormatter={(x) => (`Quarter ${x}`)}/>
-        <YAxis tickFormatter={(x) => (`$${x / 1}k`)}/>
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="value" fill="#0000FF" />
-      </BarChart>
-    )
-  }
+  // renderRechart = () => {
+  //   const rechartData = [
+  //     {name: 1, value: 1},
+  //     {name: 2, value: 2},
+  //     {name: 3, value: 3},
+  //     {name: 4, value: 0}
+  //   ]
+  //   return (
+  //     <div>
+  //       <h3>rechart</h3>
+  //       <BarChart width={730} height={250} data={rechartData}>
+  //         <CartesianGrid strokeDasharray="3 3" />
+  //         <XAxis dataKey="name" tickFormatter={(x) => (`Quarter ${x}`)}/>
+  //         <YAxis tickFormatter={(x) => (`$${x / 1}k`)}/>
+  //         <Tooltip />
+  //         <Legend />
+  //         <Bar dataKey="value" fill="#0000FF" />
+  //       </BarChart>
+  //     </div>
+  //   )
+  // }
 
   // reactVistData = [
   //   {x: 1, y: 1},
@@ -52,7 +59,7 @@ class component extends React.PureComponent {
   // ];
   // renderReactVis = () => {
   //   return (
-  //     <XYPlot width={300} height={300}>
+  //     <XYPlot width={730} height={250}>
   //       <HorizontalGridLines />
   //       <VerticalGridLines />
   //       <XAxis title="X Axis" />
@@ -73,16 +80,34 @@ class component extends React.PureComponent {
   //   )
   // }
 
+  renderReactD3 = () => {
+    const reactD3Data = [
+      { 'name': 'Series A',
+        'values': [{'x': 1, 'y': 1},
+                   {'x': 2, 'y': 2},
+                   {'x': 3, 'y': 3},
+                   {'x': 4, 'y': 0}]
+      }
+    ]
+
+    return (
+      <div>
+        <BarChart data={reactD3Data} width={730} height={250} fill={'#3182bd'}
+          title='react-d3'
+          yAxisLabel='Label'
+          xAxisLabel='Value'/>
+      </div>
+    )
+  }
+
   render () {
     return (
       <components.Box>
         <h2>
           Testing <i>charts</i> libs
         </h2>
-        <h3>Victory</h3>
         {this.renderVictoryChart()}
-        <h3>react-vis</h3>
-        {this.renderRechart()}
+        {this.renderReactD3()}
       </components.Box>
     )
   }
