db598ee0f47d4524c0555cccf004482f8d3232d8
Igor Bruno Pereira Soares <ibp.srs@gmail.com>
2018-01-29 16:14:07 -0200

    css selector for clicking analytics

--- app/src/component/analytics/clicking.js
+++ app/src/component/analytics/clicking.js
@@ -1,16 +1,66 @@
 
 const defaultSelectors = ['a', 'button', 'input', 'textarea', 'option', 'video', 'audio', 'embed', 'object']
 
-export default function setupClickingAnalytics () {
+export default function setupClickingAnalytics (extraSelectors, ignoreDefaults) {
+  var selectorArray = []
+
+  if (extraSelectors !== undefined) {
+    if (typeof extraSelectors === 'string') {
+      selectorArray = extraSelectors.split(' ').filter(i => i !== '')
+    } else if (extraSelectors instanceof Array) {
+      selectorArray = extraSelectors
+    }
+  }
+
+  if (!ignoreDefaults) {
+    selectorArray = selectorArray.concat(defaultSelectors)
+  }
+
+  var selectors = new Selectors(selectorArray)
+
   window.onclick = function (event) {
     for (var element of event.path) {
-      if (defaultSelectors.includes(element.tagName.toLowerCase())) {
+      if (selectors.match(element)) {
         console.log(element)
-        return
+        return true
       }
     }
     console.log(event)
     return true
   }
+}
+
+class Selectors {
+
+  constructor (selectorArray) {
+    this.tags = []
+    this.cssClasses = []
+    for (var selector of selectorArray) {
+      if (selector.startsWith('.')) {
+        this.cssClasses.push(selector.substr(1))
+      } else {
+        this.tags.push(selector.toLowerCase())
+      }
+    }
+  }
+
+  match (element) {
+    var tagName = element.tagName
+    if (!tagName) {
+      // an element without tag name is the document element
+      return false
+    } else if (this.tags.includes(tagName.toLowerCase())) {
+      return true
+    }
+
+    for (var cssClass of this.cssClasses) {
+      if (element.classList.contains(cssClass)) {
+        return true
+      }
+    }
+
+    return false
+  }
+
 }
 
